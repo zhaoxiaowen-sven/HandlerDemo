1fafd874e4bf1dd2737982898c7e9103d124280a
SVEN <1010773095@qq.com>
2017-08-15 12:44:15 +0800

    asynctask end

--- app/src/main/java/com/sven/handlerdemo/MainActivity.java
+++ app/src/main/java/com/sven/handlerdemo/MainActivity.java
@@ -1,22 +1,28 @@
 package com.sven.handlerdemo;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "HandlerDemo";
+
     private TextView tv;
+
+    private ProgressBar progressBar;
 
     private Handler handler1;
 
     private Handler handler2;
 
+    private MyTask myTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tv = (TextView) findViewById(R.id.tv);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        progressBar.setMax(5);
 
         handler1 = new Handler(new Handler.Callback() {
             @Override
@@ -58,7 +66,66 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onclick(View v){
-//        switch (v)
+        switch (v.getId()){
+            case R.id.update:
+                threadstart();
+                break;
+
+            case R.id.update2:
+                myTask = new MyTask();
+                myTask.execute("begin");
+                break;
+
+            case R.id.stop:
+                myTask.cancel(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    class MyTask extends AsyncTask<String, Integer, Integer>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Integer doInBackground(String... params) {
+
+            try {
+                int i = 0;
+                while (true){
+                    publishProgress(i);
+                    if (i == 5){
+                        break;
+                    }
+                    i++;
+                    Thread.sleep(1000);
+                }
+                return i;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressBar.setProgress(values[0]);
+            tv.setText("进度为: "+ values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+            tv.setText("完成了");
+        }
+    }
+
+    private void threadstart() {
         new Thread(new Runnable() {
             @Override
             public void run() {
